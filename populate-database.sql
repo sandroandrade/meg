64fa3b2e6de153f3452c15f72ab330fbfd804329
Sandro S. Andrade <sandroandrade@kde.org>
2017-07-21 22:28:19 -0300

    Add example database population script

---
+++ populate-database.sql
@@ -0,0 +1,5 @@
+insert into conferences values (1, 'QtCon-BR', 'QtCon Brasil', 'São Paulo', 'Brasil', 'Espaco Fit', '2017-08-18 09:00:00', '2017-08-20 18:00:00');
+insert into conferences values (2, 'Akademy', 'Akademy', 'Berlin', 'Germany', 'BCC', '2017-03-01 09:00:00', '2017-03-03 18:00:00');
+insert into conferences values (3, 'Conf.KDE.in', 'Conf.KDE.in', 'Guwahati', 'India', 'IIT', '2017-03-10 09:00:00', '2017-03-12 18:00:00');
+insert into speakers values (1, 'Our beloved Konqi', 'KDE', 'Konqi is awesome');
+insert into speakers values (2, 'Dirk Gently', 'Adams', 'He is a holistic detective');
